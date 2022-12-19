f9e8a442d53413f50a391ed9092119b4945d1b7a
Phoebe-Jao <jaophoebe@gmail.com>
2022-12-19 21:01:55 +0800

    feat: card badge, made badge that displays 'HOT' if review count reached 10k

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/Card.css";
 import { AiFillStar } from "react-icons/ai";
 
+<<<<<<< HEAD
 const numFormatter = function (num) {
   if (num > 999 && num < 1000000) {
     return (num / 1000).toFixed(1) + "K";
@@ -23,6 +24,24 @@ function Card({ image, altText, rating, reviewCount, title, author }) {
         </div>
         <p className="card--bookTitle">{title}</p>
         <p className="card--bookAuthor"> {author}</p>
+=======
+function Card({ image, altText, rating, reviewCount, title, author }) {
+  return (
+    <div className="card">
+      <img
+        src={image}
+        className="card--bookCover"
+        alt={ altText }
+      />
+      <div className="card--bookDetails">
+        <div className="card--bookRating">
+          <AiFillStar className="card--star" />
+          <p className="card--rating">{ rating }</p> 
+          <p className="card--reviews">({ reviewCount })</p>
+        </div>
+        <p className="card--bookTitle">{ title }</p>
+        <p className="card--bookAuthor"> {author }</p>
+>>>>>>> 4e9d24d7fb73ff3a042207af114cc059625f4351
       </div>
     </div>
   );
